b0f738077b8a7001898bd26e4d8146b5bd80f325
ian saucedo <iamasterpro1@gmail.com>
2026-02-26 13:18:08 +0100

    changing to correct gestion seguros implementation

--- Practica2/segurosgui/src/main/java/es/unican/is2/VistaAgente.java
+++ Practica2/segurosgui/src/main/java/es/unican/is2/VistaAgente.java
@@ -14,12 +14,12 @@ import java.awt.event.ActionEvent;
 import javax.swing.JList;
 import javax.swing.border.LineBorder;
 
-import es.Cliente;
+import es.unican.is2.Cliente;
 import es.unican.is2.DataAccessException;
 import es.unican.is2.IGestionClientes;
 import es.unican.is2.IGestionSeguros;
 import es.unican.is2.IInfoSeguros;
-import seguroscommon.target.Seguro;
+import es.unican.is2.Seguro;
 
 import java.awt.Color;
 import javax.swing.JButton;
--- Practica2/segurosmain/src/main/java/es/unican/is2/Runner.java
+++ Practica2/segurosmain/src/main/java/es/unican/is2/Runner.java
@@ -1,17 +1,20 @@
-import VistaAgente;
 package es.unican.is2;
+
+import es.unican.is2.VistaAgente;
 import es.unican.is2.ClientesDAO;
 import es.unican.is2.IClientesDAO;
 import es.unican.is2.ISegurosDAO;
+import es.unican.is2.SegurosDAO;
+import es.unican.is2.GestionSeguros;
 
 public class Runner {
 
-	public static void main(String[] args) {
-		IClientesDAO daoClientes = new ClientesDAO();
-		ISegurosDAO daoSeguros = new SegurosDAO();
-		GestionSeguros negocio = new GestionSeguros(daoClientes, daoSeguros);
-		VistaAgente vista = new VistaAgente(negocio, negocio, negocio);
-		vista.setVisible(true);
-	}
+    public static void main(String[] args) {
+        IClientesDAO daoClientes = new ClientesDAO();
+        ISegurosDAO daoSeguros = new SegurosDAO();
+        GestionSeguros negocio = new GestionSeguros(daoClientes, daoSeguros);
+        VistaAgente vista = new VistaAgente(negocio, negocio, negocio);
+        vista.setVisible(true);
+    }
 
 }
